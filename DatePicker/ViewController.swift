e9174ec77b4c66812bd673758fc8a927521bbca0
gorusair <gorusair@gmail.com>
2025-08-08 15:39:36 +0900

    Initial Commit

---
+++ DatePicker/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by DONGHYUN on 7/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    
+    @IBOutlet var IbICurrentTime: UILabel!
+    @IBOutlet var IbIPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval : interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        IbIPickerTime.text =
+            "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    @objc func updateTime() {
+//        IbICurrentTime.text = String(count)
+//        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        IbICurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
